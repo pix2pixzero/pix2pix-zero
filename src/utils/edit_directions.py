0d14bdc41b4c0a212d88e6d0eda470d4ad1bad6a
10maurycy10 <10maurycy10@gmail.com>
2023-02-13 19:48:18 -0800

    Implement general edit direction parser insted of hardcoding

--- src/utils/edit_directions.py
+++ src/utils/edit_directions.py
@@ -1,6 +1,11 @@
 import os
 import torch
 
+if torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+
 
 """
 This function takes in a task name and returns the direction in the embedding space that transforms class A to class B for the given task.
@@ -15,15 +20,8 @@ Examples:
 >>> construct_direction("cat2dog")
 """
 def construct_direction(task_name):
-    if task_name=="cat2dog":    
-        emb_dir = f"assets/embeddings_sd_1.4"
-        embs_a = torch.load(os.path.join(emb_dir, f"cat.pt"))
-        embs_b = torch.load(os.path.join(emb_dir, f"dog.pt"))
-        return (embs_b.mean(0)-embs_a.mean(0)).unsqueeze(0)
-    elif task_name=="dog2cat":    
-        emb_dir = f"assets/embeddings_sd_1.4"
-        embs_a = torch.load(os.path.join(emb_dir, f"dog.pt"))
-        embs_b = torch.load(os.path.join(emb_dir, f"cat.pt"))
-        return (embs_b.mean(0)-embs_a.mean(0)).unsqueeze(0)
-    else:
-        raise NotImplementedError
+    (src, dst) = task_name.split("2")
+    emb_dir = f"assets/embeddings_sd_1.4"
+    embs_a = torch.load(os.path.join(emb_dir, f"{src}.pt"), map_location=device)
+    embs_b = torch.load(os.path.join(emb_dir, f"{dst}.pt"), map_location=device)
+    return (embs_b.mean(0)-embs_a.mean(0)).unsqueeze(0)
